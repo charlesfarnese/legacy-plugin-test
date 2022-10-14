dd555560a6b904a71aabcbf579264140ece7f10e
Charles Farnese <charles@farnese.dev>
2022-10-14 11:50:25 +0100

    helmet

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import { useEffect, useState } from "react";
+import { Helmet } from "react-helmet";
 
 function LegacyPlugin() {
   const [eventData, setEventData] = useState(null);
-  const frontLegacyPlugin = () => {
-    const script = document.createElement("script");
-    script.src = "https://dl.frontapp.com/libs/frontjs.min.js";
-    script.onload = () => {
-      window.Front.on("conversation", (data) => {
-        console.log("Event data --> ", data);
-        setEventData(data);
-      });
-    };
-    document.body.append(script);
-  };
+  // const frontLegacyPlugin = () => {
+  //   const script = document.createElement("script");
+  //   script.src = "https://dl.frontapp.com/libs/frontjs.min.js";
+  //   script.onload = () => {
+  //     window.Front.on("conversation", (data) => {
+  //       console.log("Event data --> ", data);
+  //       setEventData(data);
+  //     });
+  //   };
+  //   document.body.append(script);
+  // };
 
   useEffect(() => {
-    frontLegacyPlugin();
+    window.Front.on("conversation", (data) => {
+            console.log("Event data --> ", data);
+            setEventData(data);
+          });
   }, []);
 
   return (
     <>
+      <Helmet>
+        <script
+          src="https://dl.frontapp.com/libs/frontjs.min.js"
+          integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+        />
+      </Helmet>
       <h3>Legacy Plugin</h3>
       {eventData && <div>{`event data --> ${eventData.conversation.id}`}</div>}
     </>
